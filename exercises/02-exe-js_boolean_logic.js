8b7a1b273808c5806be5099c27376083f35e98ee
Corina Perjan <cperjan@gmail.com>
2022-04-11 20:26:42 +0200

    done: boolean

--- exercises/02-exe-js_boolean_logic.js
+++ exercises/02-exe-js_boolean_logic.js
@@ -4,11 +4,11 @@
 
 let x = 5;
 let y = "5";
-console.log(`What is this: ${x == y}`); // ==> ??
+console.log(`What is this: ${x == y}`); // ==> True
 
 // === compares using value and type (strict equality)
 
-console.log(`What is this: ${x === y}`); // ==> ??
+console.log(`What is this: ${x === y}`); // ==> False
 
 // falsy values - will evaluate as FALSE
 // - false
@@ -38,9 +38,9 @@ if(true || false){
 }
 
 // Write down what will these statements return:
-console.log(true && false); // ==> ??
-console.log(11 % 3 === 2); // ==> ??
-console.log(false || true); // ==> ??
-console.log(!true || false); // ==> ??
-console.log(17 == '17'); // ==> ??
-console.log(123 === '123'); // ==> ??
+console.log(true && false); // ==> false
+console.log(11 % 3 === 2); // ==> true
+console.log(false || true); // ==> true
+console.log(!true || false); // ==> false
+console.log(17 == '17'); // ==> true
+console.log(123 === '123'); // ==> false
